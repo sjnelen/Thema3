9140571d40989f628f0b7460c06f910311b63e12
sjnelen <s.j.nelen@st.hanze.nl>
2024-08-23 16:02:13 +0200

    Added an upload function to the flask web app

--- FASTAflow/pages.py
+++ FASTAflow/pages.py
@@ -4,9 +4,14 @@ All the pages for the flask web application
 __author__ = 'Sam Nelen'
 __version__ = '2024.08.22'
 
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 
 bp = Blueprint('pages', __name__)
+
+ALLOWED_EXTENSIONS = {'fasta', 'fas', 'fa', 'fna', 'ffn', 'faa', 'mpfa', 'frn'}
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 @bp.route("/")
 def home():
@@ -15,3 +20,17 @@ def home():
 @bp.route("/about")
 def about():
     return render_template("about.html")
+
+@bp.route("/import_fasta")
+def import_fasta():
+    return render_template("import_fasta.html")
+
+@bp.route("/upload", methods=['POST'])
+def handle_upload():
+    file = request.files['filename']
+    if file and allowed_file(file.filename):
+        file.save(file.filename)
+        print(file.filename)
+        return render_template("home.html")
+    else:
+        return f"invalid filetype {file.filename}"
